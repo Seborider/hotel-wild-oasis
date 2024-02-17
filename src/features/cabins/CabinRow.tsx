af962ff61bfeb38bb2b372558579f2031ecd4c4b
Sebastian Mayer <sebastian.mayer@check24.de>
2024-02-18 00:37:48 +0100

    add react hot toasts

--- src/features/cabins/CabinRow.tsx
+++ src/features/cabins/CabinRow.tsx
@@ -1,87 +1,88 @@
 import styled from "styled-components";
-import {CabinType} from "../../interfaces.ts";
-import {formatCurrency} from "../../utils/helpers.ts";
-import {useMutation, useQueryClient} from "@tanstack/react-query";
-import {deleteCabin} from "../../services/apiCabins.ts";
+import { CabinType } from "../../interfaces.ts";
+import { formatCurrency } from "../../utils/helpers.ts";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteCabin } from "../../services/apiCabins.ts";
+import toast from "react-hot-toast";
 
 const TableRow = styled.div`
-    display: grid;
-    grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-    column-gap: 2.4rem;
-    align-items: center;
-    padding: 1.4rem 2.4rem;
+  display: grid;
+  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+  column-gap: 2.4rem;
+  align-items: center;
+  padding: 1.4rem 2.4rem;
 
-    &:not(:last-child) {
-        border-bottom: 1px solid var(--color-grey-100);
-    }
+  &:not(:last-child) {
+    border-bottom: 1px solid var(--color-grey-100);
+  }
 `;
 
 const Img = styled.img`
-    display: block;
-    width: 6.4rem;
-    aspect-ratio: 3 / 2;
-    object-fit: cover;
-    object-position: center;
+  display: block;
+  width: 6.4rem;
+  aspect-ratio: 3 / 2;
+  object-fit: cover;
+  object-position: center;
 
-    /* transform: scale(1.66666) translateX(-2px); */
-    transform: scale(1.5) translateX(-7px);
+  /* transform: scale(1.66666) translateX(-2px); */
+  transform: scale(1.5) translateX(-7px);
 `;
 
 const Cabin = styled.div`
-    font-size: 1.6rem;
-    font-weight: 600;
-    color: var(--color-grey-600);
-    font-family: Sono;
+  font-size: 1.6rem;
+  font-weight: 600;
+  color: var(--color-grey-600);
+  font-family: Sono;
 `;
 
 const Price = styled.div`
-    font-family: Sono, serif;
-    font-weight: 600;
+  font-family: Sono, serif;
+  font-weight: 600;
 `;
 
 const Discount = styled.div`
-    font-family: Sono, serif;
-    font-weight: 500;
-    color: var(--color-green-700);
+  font-family: Sono, serif;
+  font-weight: 500;
+  color: var(--color-green-700);
 `;
 
 interface CabinRowProps {
-    cabin: CabinType;
+  cabin: CabinType;
 }
 
-function CabinRow({cabin}: CabinRowProps) {
-    const {
-        image,
-        discount,
-        name,
-        maxCapacity,
-        regularPrice,
-        id: cabinId,
-    } = cabin;
-    const queryClient = useQueryClient();
+function CabinRow({ cabin }: CabinRowProps) {
+  const {
+    image,
+    discount,
+    name,
+    maxCapacity,
+    regularPrice,
+    id: cabinId,
+  } = cabin;
+  const queryClient = useQueryClient();
 
-    const {isLoading: isDeleting, mutate} = useMutation({
-        mutationFn: deleteCabin,
-        onSuccess: () => {
-            alert("Cabin successfully deleted");
-            void queryClient.invalidateQueries({
-                queryKey: ["cabins"],
-            });
-        },
-        onError: (err: Error) => alert(err.message),
-    });
-    return (
-        <TableRow>
-            <Img src={image}/>
-            <Cabin>{name}</Cabin>
-            <div>Fits up to {maxCapacity} guests</div>
-            <Discount>{formatCurrency(discount)}</Discount>
-            <Price>{formatCurrency(regularPrice)}</Price>
-            <button onClick={() => mutate(cabinId)} disabled={isDeleting}>
-                Delete Cabin
-            </button>
-        </TableRow>
-    );
+  const { isLoading: isDeleting, mutate } = useMutation({
+    mutationFn: deleteCabin,
+    onSuccess: () => {
+      toast.success("Cabin successfully deleted");
+      void queryClient.invalidateQueries({
+        queryKey: ["cabins"],
+      });
+    },
+    onError: (err: Error) => toast.error(err.message),
+  });
+  return (
+    <TableRow>
+      <Img src={image} />
+      <Cabin>{name}</Cabin>
+      <div>Fits up to {maxCapacity} guests</div>
+      <Discount>{formatCurrency(discount)}</Discount>
+      <Price>{formatCurrency(regularPrice)}</Price>
+      <button onClick={() => mutate(cabinId)} disabled={isDeleting}>
+        Delete Cabin
+      </button>
+    </TableRow>
+  );
 }
 
 export default CabinRow;
