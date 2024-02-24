3d5260cdddc76d55375b3226b9e014ca05b28eaf
Seborider <mayer.sebobo@gmail.com>
2024-02-24 17:59:13 +0100

    add filter component and use for cabins

--- src/features/cabins/CabinTable.tsx
+++ src/features/cabins/CabinTable.tsx
@@ -6,12 +6,24 @@ import ErrorFallback from "../../ui/ErrorFallback.tsx";
 import Table from "../../ui/Table.tsx";
 import { CabinType } from "../../interfaces.ts";
 import Menus from "../../ui/Menus.tsx";
+import { useSearchParams } from "react-router-dom";
 
 function CabinTable() {
   const { isLoading, cabins, error } = useCabins();
+  const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
   if (error) return <ErrorFallback />;
+
+  const filteredValue = searchParams.get("discount") ?? "all";
+
+  let filteredCabins: CabinType[] = [];
+  if (filteredValue === "all") filteredCabins = cabins ?? [];
+  if (filteredValue === "no-discount")
+    filteredCabins = cabins?.filter((cabin) => cabin.discount === 0) ?? [];
+  if (filteredValue === "with-discount")
+    filteredCabins =
+      cabins?.filter((cabin) => cabin?.discount && cabin.discount > 0) ?? [];
 
   return (
     <Menus>
@@ -28,7 +40,7 @@ function CabinTable() {
           render={(cabin: CabinType) => (
             <CabinRow key={cabin.id} cabin={cabin} />
           )}
-          data={cabins!}
+          data={filteredCabins}
         ></Table.Body>
       </Table>
     </Menus>
--- src/ui/Filter.tsx
+++ src/ui/Filter.tsx
@@ -1,4 +1,9 @@
+import { useSearchParams } from "react-router-dom";
 import styled, { css } from "styled-components";
+
+interface ButtonProps {
+  $active: boolean;
+}
 
 const StyledFilter = styled.div`
   border: 1px solid var(--color-grey-100);
@@ -10,11 +15,11 @@ const StyledFilter = styled.div`
   gap: 0.4rem;
 `;
 
-const FilterButton = styled.button`
+const FilterButton = styled.button<ButtonProps>`
   background-color: var(--color-grey-0);
   border: none;
   ${(props) =>
-    props.active &&
+    props.$active &&
     css`
       background-color: var(--color-brand-600);
       color: var(--color-brand-50);
@@ -32,3 +37,38 @@ const FilterButton = styled.button`
     color: var(--color-brand-50);
   }
 `;
+
+interface FilterProps {
+  filterField: string;
+  options: options[];
+}
+
+interface options {
+  value: string;
+  label: string;
+}
+
+function Filter({ filterField, options }: FilterProps) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentFilter = searchParams.get(filterField);
+  function handleClick(value: string) {
+    searchParams.set(filterField, value);
+    setSearchParams(searchParams);
+  }
+
+  return (
+    <StyledFilter>
+      {options.map((option) => (
+        <FilterButton
+          key={option.value}
+          onClick={() => handleClick(option.value)}
+          $active={option.value === currentFilter}
+        >
+          {option.label}
+        </FilterButton>
+      ))}
+    </StyledFilter>
+  );
+}
+
+export default Filter;
